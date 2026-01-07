6a7c6b1f166f0ddc8570e98ee0ee68c79d4b7c15
Andri Sunardi <account@andrisunardi.com>
2026-01-07 11:07:36 +0700

    Install Bootstrap 5.3.3

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <title>@yield('title') | {{ config('app.name') }}</title>
+</head>
+
+<body>
+    {{ $slot }}
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
 </html>
