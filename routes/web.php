4d1938c7a4ddc88e5e79aaafc8329595a1257ced
Andri Sunardi <account@andrisunardi.com>
2026-01-07 11:06:45 +0700

    Create Livewire Home Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('', HomePage::class)->name('home');
