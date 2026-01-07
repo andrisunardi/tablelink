12e1f5ff167654f0077b328472e25169f58e09ab
Andri Sunardi <account@andrisunardi.com>
2026-01-07 11:57:50 +0700

    Create Registration Page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\HomePage;
+use App\Livewire\RegistrationPage;
 use Illuminate\Support\Facades\Route;
 
 Route::any('', HomePage::class)->name('home');
+Route::any('registration', RegistrationPage::class)->name('registration');
